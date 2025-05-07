f4b8813f903ae4ead96f6a10cd6087a62e58e008
Cecilia HOlguín <cecilia.holguin44@outlook.es>
2025-05-06 20:12:05 -0600

    Script AvMenuPrin.sh completado

--- AvMenuPrin.sh
+++ AvMenuPrin.sh
@@ -11,28 +11,91 @@
 
 
 #Menú principal de bandera
-case $1 in
-    -a )
-        while true: do
-        opc=""
+echo "Bienvenido al Menú de Metodologías de Desarrollo de Software"
+echo "Por favor seleccione una opción:
+    *Metodologías Ágiles (presione 'a')
+    *Metodologías Tradicionales (presione 't')
+    *Salir (presione 'x')"
+read -p "Opción: " main_opc
+#Validación de la opción seleccionada
+
+case $main_opc in
+    a )
+        while true; do
             echo "Bienvenido a la guía rápida de Agile, para continuar seleccione un tema:
                 *SCRUM
                 *XP (Programación Extrema)
                 *Kanban
                 *Crystal
                 *Salir (presione 'x')"
-    ;;
-    -t ) 
-        while true: do
-        opc=""
+            read -p "Tema: " opc
+
+            case $opc in
+                SCRUM )
+                    echo "Has seleccionado SCRUM"
+                    ./AvMenuAgil.inf -a SCRUM
+                    ;;
+                XP )
+                    echo "Has seleccionado XP"
+                    ./AvMenuAgil.inf -a XP
+                    ;;
+                Kanban )
+                    echo "Has seleccionado Kanban"
+                    ./AvMenuAgil.inf -a Kanban
+                    ;;
+                Crystal )
+                    echo "Has seleccionado Crystal"
+                    ./AvMenuAgil.inf -a Crystal
+                    ;;
+                x | X )
+                    echo "Saliendo del programa..."
+                    exit 0
+                    ;;
+                * )
+                    echo "Opción no válida, intenta de nuevo, por favor"
+                    ;;
+            esac
+        done
+
+        ;;
+    t ) 
+        while true; do
             echo "Bienvenido a la guía rápida de metodologías tradicionales, para continuar seleccione un tema:
             *Cascada
             *Espiral
             *Modelo V
             *Salir (presione 'x')"
-    ;;
+            read -p "Tema: " opc
+
+            case $opc in
+                Cascada )
+                    echo "Has seleccionado Cascada"
+                    ./AvMenuTrad.inf -t Cascada
+                    ;;
+                Espiral )
+                    echo "Has seleccionado Espiral"
+                    ./AvMenuTrad.inf -t Espiral
+                    ;;
+                ModeloV )
+                    echo "Has seleccionado Modelo V"
+                    ./AvMenuTrad.inf -t ModeloV
+                    ;;
+                x | X )
+                    echo "Saliendo del programa..."
+                    exit 0
+                    ;;
+                * )
+                    echo "Opción no válida, intenta de nuevo, por favor"
+                    ;;
+            esac
+        done
+        ;;
+    x | X )
+        echo "Saliendo del programa..."
+        exit 0
+        ;;
 
     * )
         echo "Opcion no valida, intenta de nuevo, por favor"
-    ;;
+        ;;
 esac
